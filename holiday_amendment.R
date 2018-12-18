f34a8266d971a5db2785f8921f0bae463cdef71b
Suraj Sharan <surajsharan85@gmail.com>
2018-12-19 00:22:38 +0530

    Update holiday_amendment.R

--- holiday_amendment.R
+++ holiday_amendment.R
@@ -15,11 +15,15 @@ holiday_data$wday <- lubridate::wday(holiday_data$date)
 
 # holiday_data$weeks<- lubridate::week(holiday_data$date)
 
+#### Some holidays using Pacakages 
+
 holiday_data$New_year <- ifelse( as.character(holiday_data$date) %in% as.character(USNewYearsDay(2000:2025)) ,1,0)
 holiday_data$Thanksgiving_day <- ifelse( as.character(holiday_data$date) %in% as.character( USThanksgivingDay(2000:2020)) ,1,0)
 holiday_data$Labor_day <- ifelse( as.character(holiday_data$date) %in% as.character( USLaborDay(2000:2025)) ,1,0)
 holiday_data$Easter_day <- ifelse( as.character(holiday_data$date) %in% as.character( Easter(2000:2025)) ,1,0)
 
+
+#### Some holidays are coded using base packages 
 
 holiday_data$Valentine_day <- ifelse(paste(holiday_data$day,holiday_data$month) %in% paste(14,2)  ,1,0)
 holiday_data$july_4th <- ifelse(paste(holiday_data$day,holiday_data$month) %in% paste(4,7)  ,1,0)
